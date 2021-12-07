ef06bbaf1b53be104a5a9c17f9c38540163c92af
Yasha Ivanov <y291203@mail.ru>
2021-12-07 18:03:08 +0300

    update console input and output

--- NoteBook/src/info/istamendil/notebook/App.java
+++ NoteBook/src/info/istamendil/notebook/App.java
@@ -15,72 +15,71 @@ import java.util.Arrays;
  *
  * Note Book. UserInteractor and DB modules.
  * @author Alexander Ferenets (Istamendil) <ist.kazan@gmail.com>
- * 
+ *
  * Code for studying purposes. Programming course. Kazan Federal University, ITIS.
  * http://study.istamendil.info/
  */
 public class App extends Application{
-  
-  private static final String DB = "/tmp/db.txt";
-  private static final String PUNCH_CARD = "/tmp/card.txt";
-  
-  protected UserInteractor terminal;
-  protected Db db;
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    App app = new App(args);
-  }
+    private static final String DB = "C:\\Users\\ICL\\Desktop\\data\\db.txt";
+    private static final String PUNCH_CARD = "C:\\Users\\ICL\\Desktop\\data\\card.txt";
 
-  public App(String[] args) {
-    super(args);
-  }
+    protected UserInteractor terminal;
+    protected Db db;
 
-  @Override
-  public void init() {
-    try {
-      this.terminal = new PunchedCardUserInteractor(Paths.get(App.PUNCH_CARD));
-      this.db = new FileDb(App.DB);
-    } catch (UserInteractorException ex) {
-      System.out.println("Couldn't start application due error:");
-      System.err.println(ex.getMessage());
-      System.exit(1);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App(args);
     }
-  }
 
-  @Override
-  public void start() {
-    try {
-      String command;
-      while((command = this.terminal.readCommand()) != null){
-        switch(command){
-          case "readAll":
-            this.terminal.print(Arrays.toString(this.db.findAll()));
-            break;
-          case "save":
-            if((command = this.terminal.readCommand()) != null){
-              this.db.save(command);
-            }
-            break;
-          default:
-            this.terminal.print("Unkown command");
+    public App(String[] args) {
+        super(args);
+    }
+
+    @Override
+    public void init() {
+        try {
+            this.terminal = new PunchedCardUserInteractor();
+            this.db = new FileDb(App.DB);
+        } catch (UserInteractorException ex) {
+            System.out.println("Couldn't start application due error:");
+            System.err.println(ex.getMessage());
+            System.exit(1);
         }
-//        this.terminal.print(command);
-      }
-    } catch (UserInteractorReadException ex) {
-      System.out.println("Can't read user input due error:");
-      System.err.println(ex.getMessage());
-      System.exit(1);
-    } catch (UserInteractorWriteException ex) {
-      System.out.println("Can't print data to user due error:");
-      System.err.println(ex.getMessage());
-      System.exit(1);
-    } catch (DbException ex) {
-      System.err.println(ex.getMessage());
-      System.exit(1);
     }
-  }
-  
+
+    @Override
+    public void start() {
+        try {
+            String command;
+            while((command = this.terminal.readCommand()) != null){
+                switch(command){
+                    case "readAll":
+                        this.terminal.print(Arrays.toString(this.db.findAll()));
+                        break;
+                    case "save":
+                        if((command = this.terminal.readCommand()) != null){
+                            this.db.save(command);
+                        }
+                        break;
+                    default:
+                        this.terminal.print("Unknown command");
+                }
+            }
+        } catch (UserInteractorReadException ex) {
+            System.out.println("Can't read user input due error:");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        } catch (UserInteractorWriteException ex) {
+            System.out.println("Can't print data to user due error:");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        } catch (DbException ex) {
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+    }
+
 }
